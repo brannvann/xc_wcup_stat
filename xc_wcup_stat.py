4f3f37c86c5036237a97ee2157c7fc9d1e8498e6
Sergey Smirnov <gribnick32@gmail.com>
2023-01-06 14:11:24 +0300

    проход по годам, обработка этапов

--- xc_wcup_stat.py
+++ xc_wcup_stat.py
@@ -2,11 +2,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-
 
 class xcwcup_parser():
     wiki_url = "https://en.wikipedia.org"
@@ -14,11 +12,12 @@ class xcwcup_parser():
     safe_headers = {
 	'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'
     }
+    men_data = {}
 
     def __init__(self):
         pass
 
-    def get_data(self):
+    def process_data(self):
         req = Request(url = self.start_page, data=None, headers=self.safe_headers )
         html = None
         try:
@@ -47,20 +46,63 @@ class xcwcup_parser():
                             wc_urls.append(url)
                         pass
                     for url in wc_urls:
-                        self.get_year_data(url)
+                        self.process_year_data(url)
                     break
                 pass
             pass
+        else:
+            print(f"cannot read main page {self.start_page}")
         return True
 
-    def get_year_data(self, url):
+    def process_year_data(self, url):
         print(url)
-        pass
+        req = Request(url = url, data=None, headers=self.safe_headers )
+        html = None
+        try:
+            html = urlopen(req)
+        except HTTPError as err:
+            print(f"cannot read {url}, error={str(err.code)}")
+        if not html:
+            print(f"data for {url} is empty")
+            return False
+        bs = BeautifulSoup(html, 'html.parser')
+        h1title = bs.find_all('h1')[0]
+        year = h1title.getText().split('–')[0]
+        list_tbl = bs.find_all('table')
+        if (not list_tbl) or ( len(list_tbl)  < 2) :
+            print(f"cannot read table for year {year}")
+            return False
+        scedule = list_tbl[1]
+        rows = scedule.find_all('tr')
+        wc_events = {}
+        for row in rows[1:]:
+            cells = row.find_all('td')
+            for cell in cells:
+                text = cell.get_text().replace(u'\xa0', u' ').strip()
+                event_type = None
+                if ('Sprint' in text ) or (' km' in text):
+                    if ('Sprint' in text ):
+                        event_type = 'Sprint'
+                    elif ('Relay' in text):
+                        event_type = 'Relay'
+                    else:    
+                        event_type = text.split(' ')[0]
+                    if event_type:
+                        if event_type not in wc_events.keys():
+                            wc_events[event_type] = 1
+                        else:     
+                            wc_events[event_type] = wc_events[event_type] + 1
+                        break
+                    pass
+                pass
+            pass
+        self.men_data[year] = wc_events 
+        return True
 
 
 def main(args):
     m = xcwcup_parser()
-    m.get_data()
+    m.process_data()
 
 if __name__ == '__main__':
     import sys
