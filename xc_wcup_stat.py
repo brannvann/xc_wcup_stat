3dc8aac7f6813bac63a9e1ef852ea3de1aa36784
Sergey Smirnov <gribnick32@gmail.com>
2023-01-09 20:45:56 +0300

    графики

--- xc_wcup_stat.py
+++ xc_wcup_stat.py
@@ -14,7 +14,8 @@ class xcwcup_parser():
     safe_headers = {
 	'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'
     }
-    men_data = {}
+    km_data = {}
+    race_data = {}
 
     def __init__(self):
         pass
@@ -47,7 +48,6 @@ class xcwcup_parser():
                             url = f"{self.wiki_url}{link.get('href')}"
                             wc_urls.append(url)
                         pass
-                    #wc_urls.append("https://en.wikipedia.org/wiki/2022%E2%80%9323_FIS_Cross-Country_World_Cup") #lastseason hack
                     for url in wc_urls:
                         self.process_year_data(url)
                     break
@@ -78,12 +78,24 @@ class xcwcup_parser():
         scedule = list_tbl[1]
         rows = scedule.find_all('tr')
         wc_events = {}
+        race_types = {}
         for row in rows[1:]:
             cells = row.find_all('td')
             for cell in cells:
                 text = cell.get_text().replace(u'\xa0', u' ').strip()
                 event_type = None
+                race_type = 'Individual'
                 if ('Sprint' in text ) or (' km' in text):
+                    # тип гонки (спринт, преследование, скиатлон, масстарт или разделка по умолчанию)
+                    races = ['Sprint', 'Pursuit', 'Skiathlon', 'Mass Start']
+                    for race in races:
+                        if race.lower() in text.lower():
+                            race_type = race
+                    if race_type not in race_types.keys():
+                        race_types[race_type] = 1
+                    else:
+                        race_types[race_type] = race_types[race_type] + 1
+                    # длина гонки
                     if ('Sprint' in text ):
                         event_type = 'Sprint'
                     elif ('Relay' in text):
@@ -99,26 +111,40 @@ class xcwcup_parser():
                     pass
                 pass
             pass
-        self.men_data[year] = wc_events 
+        self.km_data[year] = wc_events 
+        self.race_data[year] = race_types
         return True
 
-    def save_data(self, filename):
+    def save_km_data(self, filename):
         with open(filename, "w") as ofs:
-            json.dump(self.men_data, ofs)
+            json.dump(self.km_data, ofs)
         pass
 
-    def read_data(self, filename):
+    def read_km_data(self, filename):
         try:
             with open(filename, "r") as ifs:
-                self.men_data = json.load(ifs)
+                self.km_data = json.load(ifs)
         except:
             print(f"cannot read data from {filename}")
-        pass    
+        pass   
 
-    def show_plot(self):
+    def save_ev_data(self, filename):
+        with open(filename, "w") as ofs:
+            json.dump(self.race_data, ofs)
+        pass
+
+    def read_ev_data(self, filename):
+        try:
+            with open(filename, "r") as ifs:
+                self.race_data = json.load(ifs)
+        except:
+            print(f"cannot read data from {filename}")
+        pass   
+
+    def show_km_plot(self):
         labels = []
         short_races, mid_races, long_races, marathons = [], [], [], []
-        for year, data in self.men_data.items():
+        for year, data in self.km_data.items():
             labels.append(year)
             short_r, mid_r, long_r, marathon = 0, 0, 0, 0
             for race, count in data.items():
@@ -158,14 +184,58 @@ class xcwcup_parser():
         plt.xticks(xpos, labels, rotation=60)
         plt.show()
 
+
+    def show_event_plot(self):
+        labels = []
+        individual, massstart, skiathlon, pursuit, sprint = [], [], [], [], []
+        for year, data in self.race_data.items():
+            labels.append(year)
+            ind_r, mass_r, skitln_r, purs_r, sprint_r = 0, 0, 0, 0, 0
+            for race, count in data.items():
+                if "Individual" in race:
+                    ind_r = ind_r + count
+                elif "Mass Start" in race:
+                    mass_r = mass_r + count
+                elif "Pursuit" in race:
+                    purs_r = purs_r + count
+                elif "Skiathlon" in race:
+                    skitln_r = skitln_r + count
+                elif "Sprint" in race:
+                    sprint_r = sprint_r + count
+                else:
+                    print(f"Unknown race {race}")
+            individual.append(ind_r) 
+            massstart.append(mass_r)
+            skiathlon.append(skitln_r) 
+            pursuit.append(purs_r) 
+            sprint.append(sprint_r)
+        pass
+        print(labels)
+        fig, ax = plt.subplots()
+        ax.bar(labels, individual, label='Индивидуальная гонка', color='Navy', width=1.0, edgecolor='Black')
+        ax.bar(labels, pursuit, label='Преследование', bottom=individual, color='Green', width=1.0, edgecolor='Black')
+        ax.bar(labels, skiathlon, label='Скиатлон', bottom=[ i + j for i, j in zip(individual, pursuit)], color='Blue', width=1.0, edgecolor='Black')
+        ax.bar(labels, massstart, label='Масс-старт', bottom= [ i + j + k for i, j, k in zip(individual, pursuit, skiathlon) ], color='Cyan', width=1.0, edgecolor='Black')
+        ax.bar(labels, sprint, label='Спринт', bottom= [ i + j + k + t for i, j, k, t in zip(individual, pursuit, skiathlon, massstart) ], color='Red', width=1.0, edgecolor='Black')
+        ax.set_ylabel('Количество этапов', fontsize=16)
+        ax.set_title('Формат Кубка Мира по лыжным гонкам (муж)', fontsize=16)
+        ax.legend(fontsize=16)
+        xpos = range(len(labels))
+        plt.xticks(xpos, labels, rotation=60)
+        plt.show()
+
 def main(args):
     m = xcwcup_parser()
-    filename = "xc_cup_men.json"
-    m.read_data(filename)
-    if not m.men_data:
+    km_filename = "xc_cup_men_km.json"
+    ev_filename = "xc_cup_men_ev.json"
+    m.read_km_data(km_filename)
+    m.read_ev_data(ev_filename)
+    if not m.km_data or not m.race_data:
         m.process_data()
-        m.save_data(filename)
-    m.show_plot()   
+        m.save_km_data(km_filename)
+        m.save_ev_data(ev_filename)
+    m.show_km_plot()   
+    m.show_event_plot()
     pass 
     
 if __name__ == '__main__':
